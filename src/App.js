cbce4a522a0110ae26cc4021a2733fec9023ec50
~A <anilrm07@gmail.com>
2019-05-29 16:42:04 -0700

    components.js file add in components branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Components from "./components";
 
 function App() {
   return (
     <div className="App">
+    <Components />
       <div>
         <input id="input1" type="text" />
       </div>
---
+++ src/components.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Components extends Component {
+    state = {
+        show: true
+    }
+
+    render(){
+        return(
+            <div>
+                HI
+            </div>
+        )
+    }
+}
+
+export default Components;
