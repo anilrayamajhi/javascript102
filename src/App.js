5f3d3ab7a5cafc4a9c3a2a9822dba97d4ceea2b0
Yasuhiro Nakano <ynakano@endertech.com>
2019-05-29 16:36:49 -0700

    Add input and result elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <input id="input1" type="text" />
+      </div>
+      <div>
+        2x Result: <span id="result" />
+      </div>
     </div>
   );
 }
